4faea89efec120fb281627f8e6e214d7a50e0c04
Tojiyevvd <tojiyevd33@gmail.com>
2023-05-05 23:36:21 -0700

    yangilik

---
+++ js/main.js
@@ -0,0 +1,133 @@
+function yoz1(){
+    var bt1 = document.getElementById('inp1');
+    bt1.value +=""
+}
+function yoz2(){
+    var bt2 = document.getElementById('inp1');
+    bt2.value +="("
+}
+function yoz3(){
+    var bt3 = document.getElementById('inp1');
+    bt3.value +=")"
+}
+function yoz4(){
+    var bt4 = document.getElementById('inp1');
+    bt4.value =""
+}
+function yoz5(){
+    var bt5 = document.getElementById('inp1');
+    bt5.value +="%"
+}
+function yoz6(){
+    var bt6 = document.getElementById('inp1');
+    bt6.value +=":"
+}
+function yoz7(){
+    var bt7 = document.getElementById('inp1');
+    bt7.value +=""
+}
+function yoz8(){
+    var bt8 = document.getElementById('inp1');
+    bt8.value +=")"
+}
+function yoz9(){
+    var bt9 = document.getElementById('inp1');
+    bt9.value +=")"
+}
+function yoz10(){
+    var bt10 = document.getElementById('inp1');
+    bt10.value +="TT"
+}
+function yoz11(){
+    var bt11 = document.getElementById('inp1');
+    bt11.value +="7"
+}
+function yoz12(){
+    var bt12 = document.getElementById('inp1');
+    bt12.value +="8"
+}
+function yoz13(){
+    var bt13 = document.getElementById('inp1');
+    bt13.value +="9"
+}
+function yoz14(){
+    var bt14 = document.getElementById('inp1');
+    bt14.value +="*"
+}
+function yoz15(){
+    var bt15 = document.getElementById('inp1');
+    bt15.value +="x!"
+}
+function yoz16(){
+    var bt16 = document.getElementById('inp1');
+    bt16.value +="x2"
+}
+function yoz17(){
+    var bt17 = document.getElementById('inp1');
+    bt18.value +="x3"
+}
+function yoz18(){
+    var bt18 = document.getElementById('inp1');
+    bt18.value +="xy"
+}
+function yoz19(){
+    var bt19 = document.getElementById('inp1');
+    bt19.value +="4"
+}
+function yoz20(){
+    var bt20 = document.getElementById('inp1');
+    bt20.value +="5"
+}
+function yoz21(){
+    var bt21 = document.getElementById('inp1');
+    bt21.value +="6"
+}
+function yoz22(){
+    var bt22 = document.getElementById('inp1');
+    bt22.value +="-"
+}
+function yoz23(){
+    var bt23 = document.getElementById('inp1');
+    bt23.value +="1x"
+}
+function yoz24(){
+    var bt24 = document.getElementById('inp1');
+    bt24.value +="vx"
+}
+function yoz25(){
+    var bt25 = document.getElementById('inp1');
+    bt25.value +="xvy"
+}
+function yoz26(){
+    var bt26 = document.getElementById('inp1');
+    bt26.value +="EE"
+}
+function yoz27(){
+    var bt27 = document.getElementById('inp1');
+    bt27.value +="1"
+}
+function yoz28(){
+    var bt28 = document.getElementById('inp1');
+    bt28.value +="2"
+}
+function yoz29(){
+    var bt29 = document.getElementById('inp1');
+    bt29.value +="3"
+}
+function yoz30(){
+    var bt30 = document.getElementById('inp1');
+    bt30.value +="+"
+}
+function yoz31(){
+    var bt31 = document.getElementById('inp1');
+    bt31.value +=""
+}
+function yoz35(){
+    var bt31 = document.getElementById('inp1');
+    bt31.value +="0"
+}
+function hisobla(){
+    var inp = document.getElementById('inp1');
+    console.log(inp.value);
+    inp.value = eval(inp.value);
+}
